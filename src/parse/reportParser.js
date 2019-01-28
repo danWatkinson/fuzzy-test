3db5a6f42edd53831c32cd47a6ff92c9c9a551eb
Dan <dan.watkinson@gmail.com>
2019-01-28 15:20:47 +0000

    tidying

--- src/parse/reportParser.js
+++ src/parse/reportParser.js
@@ -1,41 +1,39 @@
 const fs = require("fs");
 const mergeBackground = require('./mergeBackground');
 
-const filterResultsFrom = (jsonContent) => {
-  const features = [];
-
-  jsonContent.forEach( (feature) => {
-    const featureResults = [];
-
-    const scenariosWithBackgrounds = mergeBackground(feature.elements);
-
-    scenariosWithBackgrounds.forEach( (scenario) => {
-      const stepResults = scenario.steps.map( (step) => {
-        return step.result.status;
-      })
-
-      const scenarioResult = stepResults.reduce( (accumulator, currentValue) => {
-        return accumulator == 'failed' ? 'failed' : currentValue;
-      }, "passed");
-
-      const scenarioSummary = {name: scenario.name, result: scenarioResult};
-      featureResults.push(scenarioSummary);
-    })
-
-    features.push(featureResults);
-  })
-
-  return features;
-}
-
 module.exports = (options) => {
+
+  const scenariosFrom = (feature) => {
+    const scenarioResults = [];
+
+    mergeBackground(feature.elements).forEach( (scenario) => {
+      const scenarioResult = scenario.steps.map(step => step.result.status)
+        .reduce( (accumulator, currentValue) => {
+          return accumulator == 'failed' ? 'failed' : currentValue;
+        });
+
+      scenarioResults.push({
+        name: scenario.name,
+        result: scenarioResult
+      });
+    });
+
+    return scenarioResults;
+  }
+
+  const filterResultsFrom = (jsonContent) => {
+    const features = [];
+
+    jsonContent.forEach( (feature) => {
+      features.push(scenariosFrom(feature));
+    });
+
+    return features;
+  }
 
   const parse = (file) => {
     const content = fs.readFileSync(file);
-    const parsedContent =  JSON.parse(content);
-
-    const results = filterResultsFrom(parsedContent);
-    return results;
+    return filterResultsFrom(JSON.parse(content));
   }
 
   return Object.freeze({
