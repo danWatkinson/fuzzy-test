f2e998e4312c21f0d83cb76a98b4bbc52eb6bff6
Dan <dan.watkinson@gmail.com>
2019-03-06 12:22:45 +0000

    coverage

--- src/pipeline/tests/xray/jira/prependTestExecutionIdToAllFeatures.js
+++ src/pipeline/tests/xray/jira/prependTestExecutionIdToAllFeatures.js
@@ -3,11 +3,11 @@ const path = require('path');
 const prependFile = require('prepend-file')
 
 module.exports = (locationOfAllFeatures, executionId) => {
-  const featureFiles = fs.readdirSync(locationOfAllFeatures)
-             .filter( filename => filename.endsWith('.feature') )
-             .map( filename => path.join(locationOfAllFeatures, filename ) );
+  fs.readdirSync(locationOfAllFeatures)
+     .filter( filename => filename.endsWith('.feature') )
+     .map( filename => path.join(locationOfAllFeatures, filename ) )
+     .forEach( (featureFile) => {
+        prependFile.sync(featureFile, `@${executionId}\n`)
+     });
 
-  for(var i=0; i<featureFiles.length; i++) {
-    prependFile.sync(featureFiles[i], `@${executionId}\n`)
-  }
 }
